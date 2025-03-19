c28e34c7f6d77a08c84f713e8b9b8658ad715906
Kane Kriz <kanemkriz@gmail.com>
2025-03-19 15:09:26 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,28 @@
 // 19 March 2025
 
 
+//
+
+
 function convertToAdjList(adjMatrix) 
-{
-    return adjMatrix;
+ {
+    var adjList = [];
+
+    for (var i = 0; i < adjMatrix.length; i++) 
+    {
+        adjList[i] = []; //empty array for each encountered node
+        
+        for (var j = 0; j < adjMatrix[i].length; j++) 
+        {
+            if (adjMatrix[i][j] === 1) 
+            {
+                adjList[i].push(j); //addition of the connected node into the list, accomplishing proper functionality of the graph
+            }
+        }
+    }
+
+    return adjList;
 }
+
+//
+

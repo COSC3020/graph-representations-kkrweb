dc8b84f239ea324be7de43beb07965c0c0e18bd5
Kane Kriz <kanemkriz@gmail.com>
2025-04-06 08:22:03 -0600

    Update code.js

--- code.js
+++ code.js
@@ -29,14 +29,29 @@ function convertToAdjList(adjMatrix)
 //
 
 // Bonus function for the exercise:
-
 function convertToAdjMatrix(adjList) 
- {
- var adjMatrix = [];
-  
-  //
-  // actual functionality
-  //
-  
-  return adjMatrix;
- }
+{
+    var adjMatrix = [];
+    
+    for(var i = 0; i < adjList.length; i++) 
+    {
+        adjMatrix[i] = [];
+        
+        for(var j = 0; j < adjList.length; j++) 
+        {
+            adjMatrix[i][j] = 0;
+        }
+    }
+    
+    for(var i = 0; i < adjList.length; i++) 
+    {
+        for(var j = 0; j < adjList[i].length; j++) 
+        {
+            adjMatrix[i][adjList[i][j]] = 1; //gross?
+        }
+    }
+    
+    return adjMatrix;
+}
+
+//
